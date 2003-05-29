fa067e31453bd4c3895452a4ae118c97585463ed
dfcimm5 <unknown>
2003-05-29 14:32:12 +0000

    *** empty log message ***
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/vsn@4718 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/zzz.R
+++ R/zzz.R
@@ -2,6 +2,7 @@
 ## .First.lib: this function is called when the package is loaded
 ##-----------------------------------------------------------------
 .First.lib = function(lib, pkgname, where) {
-  require(Biobase, quietly=TRUE) || stop("Cannot load without package \"Biobase\"")  
+  require(Biobase, quietly=TRUE) || stop("Cannot load without package \"Biobase\"")
+  cat("vsn: The return type of the function vsn has changed since version 1.2.0, please see its help page.\n")
 }
 
