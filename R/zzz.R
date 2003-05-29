c98ea91ea2a78069b7394b6e64cb4bdd55b4aea9
dfcimm5 <unknown>
2003-05-29 14:32:51 +0000

    Removed the warning note
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/vsn@4719 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/zzz.R
+++ R/zzz.R
@@ -3,6 +3,5 @@
 ##-----------------------------------------------------------------
 .First.lib = function(lib, pkgname, where) {
   require(Biobase, quietly=TRUE) || stop("Cannot load without package \"Biobase\"")
-  cat("vsn: The return type of the function vsn has changed since version 1.2.0, please see its help page.\n")
 }
 
