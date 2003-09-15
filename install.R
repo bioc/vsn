36ef3e415846ca90d3f27b0dfd1f82f95fb846bc
dfcimm5 <unknown>
2003-09-15 17:26:47 +0000

    If the affy package is loaded (i.e. if the variable normalize.AffyBatch.methods is defined), vsn is now automatically registered as a normalization method for affy:AffyBatch. -- wh
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/vsn@5629 bc3139a8-67e5-0310-9ffc-ced21a209358

--- install.R
+++ install.R
@@ -1,1 +1,6 @@
 require(Biobase)
+
+## register vsn as a normalization method with the affy package, if that is loaded:
+if(exists("normalize.AffyBatch.methods"))
+  if(!"vsn" %in% normalize.AffyBatch.methods)
+    normalize.AffyBatch.methods <- c(normalize.AffyBatch.methods, "vsn")
