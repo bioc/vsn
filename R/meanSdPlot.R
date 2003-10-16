6a43098876532189e7fd28cec0999c6e3c0d5053
dfcimm5 <unknown>
2003-10-16 23:19:54 +0000

    Put marray* back into the vignette, and added the possibility to pass a matrix into meanSdPlot -- wh
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/vsn@6171 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/meanSdPlot.R
+++ R/meanSdPlot.R
@@ -1,48 +1,53 @@
-meanSdPlot = function(x, ranks=TRUE,
+meanSdPlot = function(x,
+                      ranks=TRUE,
                       xlab = ifelse(ranks, "rank(mean)", "mean"),
                       ylab = "sd",
                       pch  = ".",
-                      col  = NULL, ...) {
-  stopifnot(class(x)=="exprSet")
-
-  if(!is.logical(ranks) || is.na(ranks))
-    stop("argument ranks must be logical and not NA.")
+                      col, ...) {
+  stopifnot(is.logical(ranks), length(ranks)==1, !is.na(ranks))
 
   ## the coloring
-  sel = preproc(description(x))$vsnTrimSelection
-  if(!is.null(sel)){
-    if(!is.logical(sel) || length(sel)!=nrow(exprs(x)) || any(is.na(sel))) 
-      stop(paste("The element \"vsnTrimSelection\" of the preprocessing",
-                 "slot of the description slot of \"x\" is not valid.",
-                 "You may remove it and try again.\n"))
-    if(!is.null(col))
-      stop("Parameter \"col\" must not be specified.")
-    col = ifelse(sel, "blue", "black")
+  pcol <- "black"
+  if(missing(col)) {
+    if(inherits(x, "exprSet")) {
+      sel <- preproc(description(x))$vsnTrimSelection
+      if(!is.null(sel)){
+        if(!is.logical(sel) || length(sel)!=nrow(exprs(x)) || any(is.na(sel))) 
+          stop(paste("The element \"vsnTrimSelection\" of the preprocessing",
+                     "slot of the description slot of \"x\" is not valid.",
+                     "You may remove it and try again.\n"))
+        pcol <- ifelse(sel, "blue", "black")
+      }
+    }
   } else {
-    sel = TRUE
-    if(is.null(col))
-      col = "black"
+    pcol <- col
   }
-
-  n    = nrow(exprs(x))
-  px   = rowMeans(exprs(x), na.rm=TRUE)
-  py   = rowSds(  exprs(x), na.rm=TRUE)
-  rpx  = rank(px, na.last=FALSE)
+  
+  if(inherits(x, "exprSet"))
+    x <- exprs(x)
+  
+  if(!inherits(x, "matrix"))
+    stop("'x' must be a matrix or an exprSet (or it may inherit from these).")
+  
+  n    <- nrow(x)
+  px   <- rowMeans(x, na.rm=TRUE)
+  py   <- rowSds(  x, na.rm=TRUE)
+  rpx  <- rank(px, na.last=FALSE)
   
   ## running median with centers at dm, 2*dm, 3*dm, ... and width 2*dm
-  dm        = 0.05
-  midpoints = seq(dm, 1-dm, by=dm)
-  within    = function(x, x1, x2) { x>=x1 & x<=x2 }
-  mediwind  = function(mp) median(py[within(rpx/n, mp-dm, mp+dm)], na.rm=TRUE)
-  rq.sds    = sapply(midpoints, mediwind)
+  dm        <- 0.05
+  midpoints <- seq(dm, 1-dm, by=dm)
+  within    <- function(x, x1, x2) { x>=x1 & x<=x2 }
+  mediwind  <- function(mp) median(py[within(rpx/n, mp-dm, mp+dm)], na.rm=TRUE)
+  rq.sds    <- sapply(midpoints, mediwind)
 
   if(ranks) {
-    px  = rpx
-    pxl = midpoints*n
+    px  <- rpx
+    pxl <- midpoints*n
   } else {
-    pxl = quantile(px, probs=midpoints, na.rm=TRUE)
+    pxl <- quantile(px, probs=midpoints, na.rm=TRUE)
   }
-  plot(px, py, pch=pch, xlab=xlab, ylab=ylab, col=col, ...)
+  plot(px, py, pch=pch, xlab=xlab, ylab=ylab, col=pcol, ...)
   lines(pxl, rq.sds, col="red", type="b", pch=19)
 }
 
