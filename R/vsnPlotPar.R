274047ce0a7c6a2f4836b73a10e406a2c4319932
Wolfgang Huber <w.huber@dkfz-heidelberg.de>
2004-10-20 08:38:46 +0000

    better error message in vsnPlotPar - wh
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/vsn@10165 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/vsnPlotPar.R
+++ R/vsnPlotPar.R
@@ -4,11 +4,11 @@ vsnPlotPar = function(x, what, xlab="iter", ylab=what, ...) {
   whatopts <- c("offsets", "factors")
   j <- match(what, whatopts)
   if(is.na(j))
-    stop(paste("'what' must be one of ", whatopts, ", is: ", what, "\n", sep=""))
+    stop(paste("Argument 'what' must be one of ", whatopts, ", is: ", what, "\n", sep=""))
 
   pars = preproc(description(x))$vsnParamsIter
   if(is.null(pars) || !is.array(pars) || length(dim(pars))!=4)
-    stop("'ny' does not seem to be resulting from vsn.")
+    stop("Argument 'x' does not contain the vsn parameters!")
 
   niter <- dim(pars)[4]
   matplot(1:niter, t(matrix(pars[,,j,], ncol=niter)), type="b", pch=16, xlab=xlab, ylab=ylab, ...)
