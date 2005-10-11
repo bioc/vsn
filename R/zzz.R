0a9a13d92082fd6272a7cccb7af1e9106584dac1
Seth Falcon <sfalcon@fhcrc.org>
2005-10-11 23:34:35 +0000

    Change .First.lib to .onAttach so that it actually gets run.
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/vsn@14452 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/zzz.R
+++ R/zzz.R
@@ -1,8 +1,6 @@
-.First.lib <- function(lib, pkgname, where) {
-  ## load the compiled code
-  library.dynam(pkgname, pkgname, lib)
-  if(.Platform$OS.type == "windows" && require(Biobase) && interactive()
-        && .Platform$GUI ==  "Rgui"){
+.onAttach <- function(libname, pkgname) {
+  if(.Platform$OS.type == "windows" && require("Biobase") && interactive()
+        && .Platform$GUI ==  "Rgui") {
         addVigs2WinMenu("vsn")
     }
 }
