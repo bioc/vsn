8cdd3d8701a021202f8e78aeb844ae9b8e32b9ff
Wolfgang Huber <w.huber@dkfz-heidelberg.de>
2006-03-29 17:10:54 +0000

    removed install.R
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/vsn@16896 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/zzz.R
+++ R/zzz.R
@@ -1,6 +1,14 @@
 .onAttach <- function(libname, pkgname) {
-  if(.Platform$OS.type == "windows" && require("Biobase") && interactive()
-        && .Platform$GUI ==  "Rgui") {
+  if(.Platform$OS.type == "windows" && interactive() && .Platform$GUI ==  "Rgui") {
         addVigs2WinMenu("vsn")
     }
+}
+
+.onLoad <- function(libname, pkgname) {
+## register vsn as a normalization method with the affy package, if that is loaded:
+if ("package:affy" %in% search())
+  if(!"vsn" %in% get("normalize.AffyBatch.methods", "package:affy"))
+    assign("normalize.AffyBatch.methods",
+           c(get("normalize.AffyBatch.methods", pos="package:affy"), "vsn"),
+           pos="package:affy")
 }
--- install.R
+++
@@ -1,8 +0,0 @@
-require(Biobase)
-
-## register vsn as a normalization method with the affy package, if that is loaded:
-if ("package:affy" %in% search())
-  if(!"vsn" %in% get("normalize.AffyBatch.methods", "package:affy"))
-    assign("normalize.AffyBatch.methods",
-           c(get("normalize.AffyBatch.methods", pos="package:affy"), "vsn"),
-           pos="package:affy")
