331132246c8888719587e4b81fcfaad04173da08
Wolfgang Huber <w.huber@dkfz-heidelberg.de>
2007-03-16 18:30:32 +0000

    I have added a method for processing limma RGLists and worked on the vignette
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/vsn@23244 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/methods-justvsn.R
+++ R/methods-justvsn.R
@@ -13,7 +13,7 @@ setMethod("justvsn", "AffyBatch",
    function(x,  reference, strata, ...) {
      fit = vsnMatrix(exprs(x),  reference, strata, cvg.niter=4L,
        subsample=if(nrow(x)>30000L) 30000L else 0L, ...)
-     exprs(x) = fit@hx
+     exprs(x) = 2^fit@hx
      
       ## call RMA to do the probeset summarization,
       ## (with no background correction / normalization, that is already done)
@@ -21,13 +21,27 @@ setMethod("justvsn", "AffyBatch",
     })
 
 setMethod("justvsn", "RGList",
-   function(x,  reference, strata, backgroundsubtract=FALSE, ...) {
+   function(x,  reference, strata, backgroundsubtract=FALSE, targets, ...) {
      if(!(is.logical(backgroundsubtract)&&(length(backgroundsubtract)==1)&&(!is.na(backgroundsubtract))))
        stop("'backgroundsubtract' must be a logical of length 1 and not NA.")
+     if(!(is.matrix(x$R)&&is.matrix(x$G)&&(all(dim(x$R)==dim(x$G)))))
+       stop("Invalid 'RGList' object, slots 'R' and 'G' must be matrices of same size.")
+     y = cbind(x$R, x$G)
      
-      # fit = vsnMatrix(exprs(x),  reference, strata, ...)
+     if(backgroundsubtract) {
+       if(!(is.matrix(x$Rb)&&is.matrix(x$Gb)&&(all(dim(x$Rb)==dim(x$Gb)))&&(all(dim(x$R)==dim(x$Rb)))))
+         stop("Invalid 'RGList' object, slots 'Rb' and 'Gb' must be matrices of same size as 'R' and 'G'.")
+       y = y - cbind(x$Rb, x$Gb)
+     }
+     
+     dfc = data.frame(slide  = I(c(colnames(x$R),colnames(x$G))),
+                      colour = rep(c("R", "G"), each = ncol(x)))
+     row.names(dfc) = colnames(y) = paste(dfc$slide, dfc$colour, sep="-")
+     dfcmeta = data.frame(labelDescription = I(c("Slide name obtained from colnames of RGList$R and RGList$G", "Label colour")),
+                          row.names=c("slide", "colour"))
 
-      warning("Not Implemented")
-      
-      return(x)
-    })
+     fit = vsnMatrix(y,  reference, strata, ...)
+          
+     res = new("ExpressionSet", exprs=fit@hx, phenoData = new("AnnotatedDataFrame", data=dfc, varMetadata=dfcmeta))
+     return(res)
+   })
