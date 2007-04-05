85b3e475e67e9443862ba702e7befce11ef99f0f
Wolfgang Huber <w.huber@dkfz-heidelberg.de>
2007-04-05 14:59:42 +0000

    I update the validity functions to always return
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/vsn@23639 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/AllClasses.R
+++ R/AllClasses.R
@@ -4,35 +4,50 @@
 validScalar = function(ob, nm, min=0, max=+Inf) {
   s = slot(ob, nm)
   if((length(s)!=1)||any(is.na(s))||(s<min)||(s>max))
-    stop(sprintf("'%s' must be a scalar with values between %g and %g.", nm, min, max))
+    return(sprintf("'%s' must be a scalar with values between %g and %g.", nm, min, max))
+  TRUE
 }
+
 validLogical = function(ob, nm) {
   s = slot(ob, nm)
   if((length(s)!=1)||any(is.na(s)))
-    stop(sprintf("'%s' must be a logical of length one and not be NA.", nm))
+    return(sprintf("'%s' must be a logical of length one and not be NA.", nm))
+  TRUE
 }
 
 equalOrZero = function(i, j) ((i==j)||(i==0))
 
 validityVsnInput = function(object){
-  validScalar(object, "subsample", min=0, max=nrow(object))
-  validScalar(object, "cvg.niter", min=1)
-  validScalar(object, "cvg.eps",   min=0)
-  validScalar(object, "lts.quantile", min=0.5, max=1)
-  validLogical(object, "verbose")
-  validLogical(object, "ordered")
+  r = validScalar(object, "subsample", min=0, max=nrow(object))
+  if(!r)return(r)
+
+  r = validScalar(object, "cvg.niter", min=1)
+  if(!r)return(r)
+
+  r = validScalar(object, "cvg.eps",   min=0)
+  if(!r)return(r)
+
+  r = validScalar(object, "lts.quantile", min=0.5, max=1)
+  if(!r)return(r)
+
+  ## could also define class for this, might be a bit less tedious
+  r = validLogical(object, "verbose")
+  if(!r)return(r)
+
+  r = validLogical(object, "ordered")
+  if(!r)return(r)
 
   if(!equalOrZero(length(object@strata), nrow(object@x)))
-    stop("'length(strata)' must must match 'nrow(x)'.")
+    return("'length(strata)' must must match 'nrow(x)'.")
 
   if(any(is.na(object@strata)))
-    stop("'strata' must not contain NA values.")
+    return("'strata' must not contain NA values.")
 
   if(!is.numeric(object@pstart)||length(dim(object@pstart))!=3)
-    stop("'pstart' must be a 3D array.")
+    return("'pstart' must be a 3D array.")
   
   if(!all(dim(object@pstart)==c(nlevels(object@strata), ncol(object@x), 2)))
-    stop("Invalid dimensions of 'pstart'.")
+    return("Invalid dimensions of 'pstart'.")
 
   return(TRUE)
 }
@@ -44,35 +59,35 @@ validityVsnInput = function(object){
 ## If any these slots is of length>0, then they must agree in size.
 validityVsn = function(object){
   if(any(is.na(object@par))||(length(dim(object@par))!=3))
-    stop("'par' must be a 3D array and not contain NA values.")
+    return("'par' must be a 3D array and not contain NA values.")
 
   if(dim(object@par)[3]!=2)
-    stop("'dim(par)[3]' must be equal to 2.")
+    return("'dim(par)[3]' must be equal to 2.")
   
   if((length(object@n)!=1)||any(is.na(object@n)))
-    stop("'n' must be of length 1 and not NA.")
+    return("'n' must be of length 1 and not NA.")
 
-  if(any(is.na(object@refh)))
-    warning("'refh' contains NA.")
+##  if(any(is.na(object@refh)))
+##    warning("'refh' contains NA.")
   
   if(length(object@refsigma)!=1)
-    stop("'refsigma' must be of length 1.")
+    return("'refsigma' must be of length 1.")
 
   if(!equalOrZero(ncol(object@hx), dim(object@par)[2]))
-    stop("'ncol(hx)' and 'dim(object@par)[2]' must match.")
+    return("'ncol(hx)' and 'dim(object@par)[2]' must match.")
 
   if(!equalOrZero(length(object@strata), object@n))
-    stop("'length(strata)' must match 'n'.")
+    return("'length(strata)' must match 'n'.")
 
   if(!equalOrZero(length(object@refh), object@n))
-    stop("'length(refh)' must match 'n'.")
+    return("'length(refh)' must match 'n'.")
 
   if(!equalOrZero(nrow(object@hx), object@n))
-    stop("'nrow(hx)' must match 'n'.")
+    return("'nrow(hx)' must match 'n'.")
 
   if(length(object@strata)>0)
     if(nlevels(object@strata)!=dim(object@par)[1])
-      stop("'nlevels(strata)' and 'dim(par)[1]' must match.")
+      return("'nlevels(strata)' and 'dim(par)[1]' must match.")
   
   return(TRUE)
 }
