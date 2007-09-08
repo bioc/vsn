6a10f8bc61003d40797fc56a4155e1f006d6be2c
Wolfgang Huber <w.huber@dkfz-heidelberg.de>
2007-09-08 20:03:14 +0000

    rowV (which is also used by meanSdPlot) now uses (n-1) in the denominator of variance estimator rather than n
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/vsn@26963 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/vsn2.R
+++ R/vsn2.R
@@ -368,10 +368,10 @@ scalingFactorTransformation = function(b) {
 rowV = function(x, mean, ...) {
   sqr     = function(x)  x*x
   n       = rowSums(!is.na(x))
-  n[n<1] = NA
+  n[n<1]  = NA
   if(missing(mean))
     mean=rowMeans(x, ...)
-  return(rowSums(sqr(x-mean), ...)/n)
+  return(rowSums(sqr(x-mean), ...)/(n-1))
 }
 
 ##--------------------------------------------------------------
