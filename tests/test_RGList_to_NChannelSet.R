de86becab75cd0852deda0fe483c4177d2167b98
Wolfgang Huber <w.huber@dkfz-heidelberg.de>
2007-09-08 19:36:00 +0000

    minor improvements
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/vsn@26959 bc3139a8-67e5-0310-9ffc-ced21a209358

--- tests/test_RGList_to_NChannelSet.R
+++ tests/test_RGList_to_NChannelSet.R
@@ -4,10 +4,10 @@
 ##  path and that the source directories for these packages are
 ##  found underneath. Let's see for many build systems that is true.
 
-p = strsplit(getwd(), .Platform$file.sep)[[1]]
+p = strsplit(getwd(), .Platform$file.sep)[[1L]]
 w1 = which(p=="madman")
 w2 = which(p=="Rpacks")
-stopifnot(length(w1)==1L, length(w2)==1L)
+stopifnot(length(w1)==1L, length(w2)==1L, w2==w1+1L)
 rt = paste(p[1L:w2], collapse= .Platform$file.sep)
 zup = readLines(file.path(rt, "vsn",     "R", "RGList_to_NChannelSet.R"))
 zap = readLines(file.path(rt, "convert", "R", "RGList_to_NChannelSet.R"))
