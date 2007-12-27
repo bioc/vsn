1b070c70b18b5eb0538d863e427cafccdaf99c8c
Wolfgang Huber <w.huber@dkfz-heidelberg.de>
2007-12-27 22:11:43 +0000

    A new version for the function normalize.AffyBatch.vsn from David Kreil.
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/vsn@29332 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/normalize.AffyBatch.vsn.R
+++ R/normalize.AffyBatch.vsn.R
@@ -1,42 +1,37 @@
 ##------------------------------------------------------------
 ## a wrapper for vsn to be used as a normalization method in
 ## the package affy
-##------------------------------------------------------------
 ## D P Kreil <bioc07@kreil.org>
-normalize.AffyBatch.vsn<-function(abatch,
-                                  reference,
-                                  strata=NULL,
-                                  subsample=30000L,
-                                  subset,
-                                  log2scale=TRUE,
-                                  log2asymp=FALSE,
-                                  ...) {
-  require(affy);
-  if (missing(subset)) {
-    ind<-unlist(indexProbes(abatch,"pm"));
-  } else {
-    ind<-unlist(indexProbes(abatch,"pm")[subset]);
-  }
-  if (!is.na(subsample)) {
-    if (!is.numeric(subsample))
-      stop("'subsample' must be numeric.");
-    if (length(ind)>subsample)
-      ind<-sample(ind,subsample);
-  }
-  if (length(strata)>0) {  # does this make sense for affy? was at probe level!
-    stop("normalize.AffyBatch.vsn: strata not supported");
-#    strind<-rep(as.integer(1), length(ind));
-#    strata=factor(integer(0), levels="all");
-  }
-  vsn2res<-vsn2(intensity(abatch)[ind,],reference=reference,
-                returnData=FALSE,...);
+##------------------------------------------------------------
+normalize.AffyBatch.vsn=function(abatch,
+                                 reference,
+                                 strata=NULL,
+                                 subsample=30000L,
+                                 subset,
+                                 log2scale=TRUE,
+                                 log2asymp=FALSE,
+                                 ...) {
+
+  if(is.na(log2scale)||is.na(log2asymp)||(log2scale&&log2asymp))
+    stop("'log2asymp' and 'log2scale' must not both be TRUE, and not be NA.")
+  
+  ind = indexProbes(abatch,"pm")
+  if(!missing(subset))
+    ind = ind[subset]
+  ind = unlist(ind)
+       
+  vsn2res = vsn2(intensity(abatch)[ind,],reference=reference,
+                 returnData=FALSE,subsample=subsample,...)
+  
   description(abatch)@preprocessing = c(description(abatch)@preprocessing, 
-                       list(vsnReference=vsn2res));
-  trsfx = predict(vsn2res,newdata=intensity(abatch),log2scale=log2scale);
-  if (log2asymp) { ## irrelevant affine transformation for ~ log2(x) for x>>1
-    if (log2scale) stop("log2asymp==TRUE implies log2scale==FALSE.");
-    trsfx<-(trsfx-log(2))/log(2);
-  }
-  intensity(abatch)<-2^trsfx;
-  return(abatch);
+                       list(vsnReference=vsn2res))
+  
+  trsfx = predict(vsn2res,newdata=intensity(abatch),log2scale=log2scale)
+
+  ## irrelevant affine transformation for ~ log2(x) for x>>1
+  if (log2asymp)
+    trsfx=(trsfx-log(2))/log(2)
+   
+  intensity(abatch)<-2^trsfx
+  return(abatch)
 }
