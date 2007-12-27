26416d6f95d633e3dc891223ae47f5020a4f7c6f
Wolfgang Huber <w.huber@dkfz-heidelberg.de>
2007-12-27 21:35:56 +0000

    I replaced the function normalize.AffyBatch.vsn by one submitted by David Kreil
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/vsn@29330 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/normalize.AffyBatch.vsn.R
+++ R/normalize.AffyBatch.vsn.R
@@ -2,37 +2,41 @@
 ## a wrapper for vsn to be used as a normalization method in
 ## the package affy
 ##------------------------------------------------------------
-normalize.AffyBatch.vsn = function (abatch, subsample = 20000, niter = 4, strata,...)  
-{
-  if (!("package:affy" %in% search())) 
-    stop("Please load the package affy before calling normalize.AffyBatch.vsn")
-
-  ## ind = the perfect match probes. If length(ind) is larger than the value in
-  ## subsample, then only use a random sample of size subsample from these
-  ind = unlist(indexProbes(abatch, "pm"))
-  if (!is.na(subsample)) {
-    if (!is.numeric(subsample)) 
-      stop("'subsample' must be numeric.")
-    if (length(ind) > subsample) 
-      ind = sample(ind, subsample)
+## D P Kreil <bioc07@kreil.org>
+normalize.AffyBatch.vsn<-function(abatch,
+                                  reference,
+                                  strata=NULL,
+                                  subsample=30000L,
+                                  subset,
+                                  log2scale=TRUE,
+                                  log2asymp=FALSE,
+                                  ...) {
+  require(affy);
+  if (missing(subset)) {
+    ind<-unlist(indexProbes(abatch,"pm"));
+  } else {
+    ind<-unlist(indexProbes(abatch,"pm")[subset]);
   }
-
-  if(missing(strata))
-    strind <- rep(as.integer(1), length(ind))
-  else 
-    strind <- strata[ind]
-  
-  ## call parameter estimation (on subset of data)
-  vsnres = vsn(intensity(abatch)[ind, ], niter=niter, strata=strind, ...)
-
-  ## add parameters to preprocessing slot
-  pars = preproc(description(vsnres))$vsnParams
-  parsIter = preproc(description(vsnres))$vsnParamsIter # J.: added 2005/04/26
-  description(abatch)@preprocessing = c(description(abatch)@preprocessing, list(vsnParams=pars, vsnParamsIter=parsIter))
-
-  ## apply the transformation (to all data)
-  intensity(abatch) = exp(vsnh(intensity(abatch), pars, strata=strata))
-  return(abatch)
+  if (!is.na(subsample)) {
+    if (!is.numeric(subsample))
+      stop("'subsample' must be numeric.");
+    if (length(ind)>subsample)
+      ind<-sample(ind,subsample);
+  }
+  if (length(strata)>0) {  # does this make sense for affy? was at probe level!
+    stop("normalize.AffyBatch.vsn: strata not supported");
+#    strind<-rep(as.integer(1), length(ind));
+#    strata=factor(integer(0), levels="all");
+  }
+  vsn2res<-vsn2(intensity(abatch)[ind,],reference=reference,
+                returnData=FALSE,...);
+  description(abatch)@preprocessing = c(description(abatch)@preprocessing, 
+                       list(vsnReference=vsn2res));
+  trsfx = predict(vsn2res,newdata=intensity(abatch),log2scale=log2scale);
+  if (log2asymp) { ## irrelevant affine transformation for ~ log2(x) for x>>1
+    if (log2scale) stop("log2asymp==TRUE implies log2scale==FALSE.");
+    trsfx<-(trsfx-log(2))/log(2);
+  }
+  intensity(abatch)<-2^trsfx;
+  return(abatch);
 }
-
-
