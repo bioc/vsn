4812b53f8e8fc03e1a4176c744fc924cade74d0a
Wolfgang Huber <w.huber@dkfz-heidelberg.de>
2008-06-04 21:47:57 +0000

    removed unnecessary testing of .Platform$OS.type etc. before the call to addVigs2WinMenu.
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/vsn@32137 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/zzz.R
+++ R/zzz.R
@@ -1,12 +1,8 @@
 .onAttach <- function(libname, pkgname) {
-  if(.Platform$OS.type == "windows" && interactive() && .Platform$GUI ==  "Rgui") {
-        addVigs2WinMenu("vsn")
-    }
+   addVigs2WinMenu("vsn")
 }
 
 .onLoad <- function(libname, pkgname) {
-  ## load methods package
-  require("methods")
   ## register vsn as a normalization method with the affy package, if that is loaded:
   if ("package:affy" %in% search())
     if(!"vsn" %in% get("normalize.AffyBatch.methods", "package:affy"))
