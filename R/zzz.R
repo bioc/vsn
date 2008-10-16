3f201de40de404e9ccfc530b62177229f6b36a75
Robert Gentleman <rgentlem@fhcrc.org>
2008-10-16 23:18:19 +0000

    updating vsn to be in line with the new affy package mostly cosmetic, but the affybatch.example data is gone
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/vsn@34609 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/zzz.R
+++ R/zzz.R
@@ -5,8 +5,7 @@
 .onLoad <- function(libname, pkgname) {
   ## register vsn as a normalization method with the affy package, if that is loaded:
   if ("package:affy" %in% search())
-    if(!"vsn" %in% get("normalize.AffyBatch.methods", "package:affy"))
-      assign("normalize.AffyBatch.methods",
-             c(get("normalize.AffyBatch.methods", pos="package:affy"), "vsn"),
-             pos="package:affy")
+    if(!"vsn" %in% affy::normalize.AffyBatch.methods())
+      upDate.normalize.AffyBatch.methods(
+             c(affy::normalize.AffyBatch.methods(), "vsn"))
 }
