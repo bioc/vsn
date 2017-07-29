dad0bb6be3236aa2e09556438f17429033410699
Wolfgang Huber <w.huber@dkfz-heidelberg.de>
2017-07-29 12:47:54 +0000

    Removed zombie code related to the first generation of vsn (they were deprecated since around 2005), now vsn2.
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/vsn@131648 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/meanSdPlot-methods.R
+++ R/meanSdPlot-methods.R
@@ -1,45 +1,46 @@
 ## ==========================================================================
 ## meanSdPlot method for matrix
 ## - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-setMethod("meanSdPlot", signature="matrix", definition =
-    function(x, ranks=TRUE, xlab = ifelse(ranks, "rank(mean)", "mean"),
+setMethod("meanSdPlot", signature = "matrix", definition =
+    function(x, ranks = TRUE, xlab = ifelse(ranks, "rank(mean)", "mean"),
              ylab = "sd", pch, plot = TRUE, bins = 50, ...) {
       
-      stopifnot(is.logical(ranks), length(ranks)==1, !is.na(ranks))
+      stopifnot(is.logical(ranks), length(ranks) == 1, !is.na(ranks))
 
       n = nrow(x)
       if (n == 0L) {
-        warning("In 'meanSdPlot': matrix has 0 rows, there is nothing to be done.")
+        warning("In 'meanSdPlot': input matrix 'x' has 0 rows. There is nothing to be done.")
         return()
       }
       if (!missing(pch)) {
         warning("In 'meanSdPlot': 'pch' is ignored.")
       }
       
-      px   = rowMeans(x, na.rm=TRUE)
-      py   = sqrt(rowV(x, mean=px, na.rm=TRUE))
-      rpx  = rank(px, na.last=FALSE, ties.method = "random")
+      px   = rowMeans(x, na.rm = TRUE)
+      py   = sqrt(rowV(x, mean = px, na.rm = TRUE))
+      rpx  = rank(px, na.last = FALSE, ties.method = "random")
                 
       ## run median with centers at dm, 2*dm, 3*dm,... and width 2*dm
       dm        = 0.025
       midpoints = seq(dm, 1-dm, by = dm)
-      within    = function(x, x1, x2) { x>=x1 & x<=x2 }
-      mediwind  = function(mp) median(py[within(rpx/n, mp-2*dm, mp+2*dm)], na.rm=TRUE)
+      within    = function(x, x1, x2) { (x >= x1) & (x <= x2) }
+      mediwind  = function(mp) median(py[within(rpx/n, mp - 2*dm, mp + 2*dm)], na.rm = TRUE)
       rq.sds    = sapply(midpoints, mediwind)
                 
       res = if(ranks) {
-        list(rank=midpoints*n, sd=rq.sds, px=rpx, py=py)
+        list(rank = midpoints*n, sd = rq.sds, px = rpx, py = py)
       } else {
-        list(quantile=quantile(px, probs=midpoints, na.rm=TRUE), sd=rq.sds, px=px, py=py)
+        list(quantile = quantile(px, probs = midpoints, na.rm = TRUE), sd = rq.sds, px = px, py = py)
       }
       
       fmt = function() function(x) format(round(x, 0), nsmall = 0L, scientific = FALSE)
 
       res$gg = ggplot(data.frame(px = res$px, py = res$py),
-            aes(x = px, y = py)) + xlab(xlab) + ylab(ylab) +
-            stat_binhex(bins = bins, ...) +
+            aes_string(x = "px", y = "py")) + xlab(xlab) + ylab(ylab) +
+            geom_hex(bins = bins, ...) +
             scale_fill_gradient(name = "count", trans = "log", labels = fmt()) + 
-            geom_line(aes(x = x, y = y), data = data.frame(x = res[[1]], y = res$sd), color = "red")
+          geom_line(aes_string(x = "x", y = "y"),
+                    data = data.frame(x = res[[1]], y = res$sd), color = "red")
               
       if (plot) print(res$gg)
       
